89dbea695c5096d869d4045363d053b494f8f598
felipe.moreira <felipe.moreira@neogrid.com>
2023-09-05 10:41:34 -0300

    primeira aula de js

---
+++ Exercicios/javascript/aula-01.js
@@ -0,0 +1,55 @@
+//codigo binario
+//a 010101010100101010101010101010
+//b 0101000101010101010110110101010101
+//c 0101010101010101010101010101010101010010110
+//d 01010101010101010101
+//console.log("Meu nome é:" + nome + " Tenho:" + idade + " anos")
+let sobrenome = "Moreira"
+let sexo = "Masculino"
+//console.log(sobrenome)
+sobrenome = "Joao"
+//console.log("Meu nome é:" + nome + sobrenome + " Tenho:" + idade + " anos")
+
+//console.log(sobrenome)
+let count = 0
+count = 1
+
+//console.log(count)
+
+let
+and
+const nome = "Felipe"
+const idade = 31
+
+// Principais tipos de Dados
+String
+Number
+Boolean
+Array
+Object
+
+String  =  "Texto qualquer"
+Number = 5
+Boolean = true / false
+Array = [0, 5, 10, 35] //0 , 1 , 2 , 3 , 4 Array de numbers
+Array = ["Felipe", "Julia", "André", "Fabiana"] //0 , 1 , 2 , 3 , 4 Array de numbers
+//let nomes ="Felipe Julia Andre Galvao Fabiana"
+//let nomes = ["Felipe", "Julia", "André", "Fabiana"]
+//let funcionario = ["Felipe Moreira", "Pedreiro", 4800, "Homen", "Ensino medio Completo", "Casado"]
+let funcionario = {
+    nome: "Felipe Moreira",
+    profissao: "Pedreiro",
+    salario: 4800,
+    genero: "Homen",
+    escolaridade: "Ensino medio completo",
+    estadoCivil: "Casado",
+   
+}
+console.log(funcionario.nome)
+
+let valorA =  10
+let valorB = 15
+
+let soma = valorA+valorB
+
+console.log(soma)
